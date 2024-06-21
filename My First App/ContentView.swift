e7f5d9c361b78e93dc5416ab45ce487a6d22ca7c
Isabella Gomez Rodriguez <isabellagomezr@gmail.com>
2024-06-21 13:23:46 -0400

    Initial Commit

---
+++ My First App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  My First App
+//
+//  Created by Isabella Gomez Rodriguez on 6/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ My First App/My_First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_AppApp.swift
+//  My First App
+//
+//  Created by Isabella Gomez Rodriguez on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct My_First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
